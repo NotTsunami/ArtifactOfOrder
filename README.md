9eb4821cd0a020176a4dd25cd79988c2632af715
NotTsunami <4589807+NotTsunami@users.noreply.github.com>
2020-02-06 00:43:00 -0500

    Rename and update to 1.0.2
    * Don't sequence on single-player games without a Dio's Best Friend
    * Rename to SequenceOnDeath

--- README.md
+++ README.md
@@ -1,10 +1,19 @@
-# ResequenceOnDeath
-A Risk of Rain 2 mod that applies the inventory resequencing of a Shrine of Order on player deaths.
+# SequenceOnDeath
+A Risk of Rain 2 mod that applies the inventory sequencing of a Shrine of Order on player deaths.
 
 ## Features
-This mod applies the same inventory resequencing effect that a Shrine of Order applies on a player death. It requires a player to have at least 2 items of any tier.
+This mod applies the same inventory sequencing effect that a Shrine of Order applies on a player death, similar to the old [OrderOnDeath](https://thunderstore.io/package/Kintelligence/OrderOnDeath/) mod. You must either be in a multiplayer game OR have a Dio's Best Friend.
 
 ## Installation
 1. Install [BepInEx](https://thunderstore.io/package/bbepis/BepInExPack/) and [R2API](https://thunderstore.io/package/tristanmcpherson/R2API/).
-2. Copy the included `ResequenceOnDeath.dll` into the resulting `C:\Program Files (x86)\Steam\steamapps\common\Risk of Rain 2\BepInEx\plugins` folder.
+2. Copy the included `SequenceOnDeath.dll` into the resulting `C:\Program Files (x86)\Steam\steamapps\common\Risk of Rain 2\BepInEx\plugins` folder.
 3. Launch the game and enjoy!
+
+## Changelog
+### Version 1.0.2
+- Build against latest R2API
+- Don't sequence on single-player games without a Dio's Best Friend
+- Rename to SequenceOnDeath
+
+### Version 1.0.1
+- Only apply to players, not monsters
---
+++ SequenceOnDeath/SequenceOnDeath.cs
@@ -0,0 +1,45 @@
+﻿using BepInEx;
+using RoR2;
+using UnityEngine;
+
+namespace Tsunami
+{
+    [BepInDependency("com.bepis.r2api")]
+    [BepInPlugin("dev.tsunami.SequenceOnDeath", "SequenceOnDeath", "1.0.2")]
+    public class SequenceOnDeath : BaseUnityPlugin
+    {
+        public void Awake()
+        {
+            On.RoR2.CharacterBody.OnDeathStart += (orig, self) =>
+            {
+                // Check to see if we are a player, otherwise this is applied to monsters as well
+                if (self.isPlayerControlled)
+                {
+                    // Check if we're in multiplayer OR have an unused Dio's Best Friend
+                    // This avoids sequencing on a single player run without a Dio's Best Friend
+                    if (PlayerCharacterMasterController.instances.Count > 1 || (self.inventory.GetItemCount(ItemIndex.ExtraLife) > 0))
+                    {
+                        // Sequence 
+                        self.inventory.ShrineRestackInventory(new Xoroshiro128Plus(Run.instance.stageRng.nextUlong));
+
+                        // Mimic the original Shrine of Order chat broadcast and effect
+                        Chat.SendBroadcastChat(new Chat.SubjectFormatChatMessage
+                        {
+                            subjectAsCharacterBody = self,
+                            baseToken = "SHRINE_RESTACK_USE_MESSAGE"
+                        });
+                        EffectManager.SpawnEffect(Resources.Load<GameObject>("Prefabs/Effects/ShrineUseEffect"),
+                            new EffectData
+                            {
+                                origin = self.footPosition,
+                                rotation = Quaternion.identity,
+                                scale = 1f,
+                                color = new Color(1f, 0.23f, 0.6337214f)
+                            }, true);
+                    }
+                }
+                orig(self);
+            };
+        }
+    }
+}
