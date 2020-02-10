4366f8885a57245c7658ead3548336128389cdde
NotTsunami <4589807+NotTsunami@users.noreply.github.com>
2020-02-10 15:02:20 -0500

    Fix Dio's Best Friend interactions

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 A Risk of Rain 2 mod that applies the inventory sequencing of a Shrine of Order on player deaths.
 
 ## Features
-This mod applies the same inventory sequencing effect that a Shrine of Order applies on a player death, similar to the old [OrderOnDeath](https://thunderstore.io/package/Kintelligence/OrderOnDeath/) mod. You must either be in a multiplayer game OR have a Dio's Best Friend.
+This mod applies the same inventory sequencing effect that a Shrine of Order applies on a player death, similar to the old [OrderOnDeath](https://thunderstore.io/package/Kintelligence/OrderOnDeath/) mod. You must either be in a multiplayer game OR have a Dio's Best Friend for this mod to take effect.
 
 ## Installation
 1. Install [BepInEx](https://thunderstore.io/package/bbepis/BepInExPack/) and [R2API](https://thunderstore.io/package/tristanmcpherson/R2API/).
@@ -10,6 +10,9 @@ This mod applies the same inventory sequencing effect that a Shrine of Order app
 3. Launch the game and enjoy! To remove you simply need to delete the `SequenceOnDeath.dll` file.
 
 ## Changelog
+### Version 1.1.0
+- Fix Dio's Best Friend interactions
+
 ### Version 1.0.2
 - Build against latest R2API
 - Don't sequence on single-player games without a Dio's Best Friend
--- SequenceOnDeath/SequenceOnDeath.cs
+++ SequenceOnDeath/SequenceOnDeath.cs
@@ -5,41 +5,50 @@ using UnityEngine;
 namespace Tsunami
 {
     [BepInDependency("com.bepis.r2api")]
-    [BepInPlugin("dev.tsunami.SequenceOnDeath", "SequenceOnDeath", "1.0.2")]
+    [BepInPlugin("dev.tsunami.SequenceOnDeath", "SequenceOnDeath", "1.1.0")]
     public class SequenceOnDeath : BaseUnityPlugin
     {
         public void Awake()
         {
-            On.RoR2.CharacterBody.OnDeathStart += (orig, self) =>
+            On.RoR2.CharacterMaster.OnBodyDeath += (orig, self, body) =>
             {
-                // Check to see if we are a player, otherwise this is applied to monsters as well
-                if (self.isPlayerControlled)
-                {
-                    // Check if we're in multiplayer OR have an unused Dio's Best Friend
-                    // This avoids sequencing on a single player run without a Dio's Best Friend
-                    if (PlayerCharacterMasterController.instances.Count > 1 || (self.inventory.GetItemCount(ItemIndex.ExtraLife) > 0))
-                    {
-                        // Sequence 
-                        self.inventory.ShrineRestackInventory(new Xoroshiro128Plus(Run.instance.stageRng.nextUlong));
+                // The original function removes a Dio's Best Friend from the inventory if one is available,
+                // so the easiest way to track usage is to compare the count before and after the function
+                int dioCount = GetDioCount(self);
 
-                        // Mimic the original Shrine of Order chat broadcast and effect
-                        Chat.SendBroadcastChat(new Chat.SubjectFormatChatMessage
-                        {
-                            subjectAsCharacterBody = self,
-                            baseToken = "SHRINE_RESTACK_USE_MESSAGE"
-                        });
-                        EffectManager.SpawnEffect(Resources.Load<GameObject>("Prefabs/Effects/ShrineUseEffect"),
-                            new EffectData
-                            {
-                                origin = self.footPosition,
-                                rotation = Quaternion.identity,
-                                scale = 1f,
-                                color = new Color(1f, 0.23f, 0.6337214f)
-                            }, true);
-                    }
-                }
-                orig(self);
+                orig(self, body);
+
+                // Bail if we're single player without a Dio's Best Friend
+                if (!IsMultiplayer() && dioCount == GetDioCount(self)) return;
+
+                // Sequence the inventory
+                self.inventory.ShrineRestackInventory(new Xoroshiro128Plus(Run.instance.stageRng.nextUlong));
+
+                // Mimic the original Shrine of Order chat broadcast and effect
+                Chat.SendBroadcastChat(new Chat.SubjectFormatChatMessage
+                {
+                    subjectAsCharacterBody = body,
+                    baseToken = "SHRINE_RESTACK_USE_MESSAGE"
+                });
+                EffectManager.SpawnEffect(Resources.Load<GameObject>("Prefabs/Effects/ShrineUseEffect"), 
+                    new EffectData
+                    {
+                        origin = body.footPosition,
+                        rotation = Quaternion.identity,
+                        scale = 1f,
+                        color = new Color(1f, 0.23f, 0.6337214f)
+                    }, true);
             };
+        }
+
+        private static bool IsMultiplayer()
+        {
+            return PlayerCharacterMasterController.instances.Count > 1;
+        }
+
+        private static int GetDioCount(CharacterMaster master)
+        {
+            return master.inventory.GetItemCount(ItemIndex.ExtraLife);
         }
     }
 }
